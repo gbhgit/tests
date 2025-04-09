c38c821f5adc7916f8c5840636aefaf00947e4a2
gbhgit <gabrielbandeiraholanda@gmail.com>
2025-04-09 15:00:11 -0300

    change the test

--- django-api/measurements/serializers.py
+++ django-api/measurements/serializers.py
@@ -1,7 +1,8 @@
 from measurements.models import Measurement
 from rest_framework import serializers
 
+
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = '__all__'
+        fields = "__all__"
